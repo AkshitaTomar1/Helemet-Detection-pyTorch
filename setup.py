d6e2621eb58f0536f503c53fd8b82f234cb3f3e5
AkshitaTomar1 <axtomar7@gmail.com>
2025-04-13 21:29:16 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="helmet-detection",
+    version="0.0.1",
+    author="Akshita",
+    author_email="axtomar7@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
